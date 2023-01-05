cc5d14165c02c50f02704c304ce1249c47990352
weili-git <244385319@qq.com>
2023-01-05 14:14:18 +0900

    23-1-5

--- 143_list.py
+++ 143_list.py
@@ -11,3 +11,40 @@ class Solution:
         if head is None or head.next is None:
             return
         
+        slow = head
+        fast = head
+        while fast.next and fast.next.next: # 1. find middle
+            slow = slow.next
+            fast = fast.next.next
+
+        needReverser = slow.next # 2. reverse
+        slow.next = None
+        needReverser = reverse(needReverser)
+
+        cur = head
+        while cur and needReverser: # 3. traverse
+            curSecond = needReverser
+            needReverser = needReverser.next
+            nextCur = cur.next
+            curSecond.next = cur.next
+            cur.next = curSecond
+
+            cur = nextCur
+            # cur_n = cur.next
+            # rev_n = needReverser.next
+            # cur.next = needReverser
+            # needReverser.next = cur_n
+            # cur = cur_n
+            # needReverser = Rev_n
+
+
+    def reverse(self, head):
+        p1 = None
+        p2 = head
+        p3 = p2
+        while p2:
+            p3 = p2.next
+            p2.next = p1
+            p1 = p2
+            p2 = p3
+        return p1
